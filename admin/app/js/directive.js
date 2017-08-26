10b85b70a17be085e7d3f53ede5a1001d1ea3b92
ahmdabos <ahmdabos@gmail.com>
2017-08-26 21:53:09 +0400

    sass colors variables

--- admin/app/js/directive.js
+++ admin/app/js/directive.js
@@ -35,22 +35,21 @@ angular.module('app')
         }
     ])
     //upload image preview
-    .directive('ngThumb', ['$window', function($window) {
+    .directive('ngThumb', ['$window', function ($window) {
         var helper = {
             support: !!($window.FileReader && $window.CanvasRenderingContext2D),
-            isFile: function(item) {
+            isFile: function (item) {
                 return angular.isObject(item) && item instanceof $window.File;
             },
-            isImage: function(file) {
-                var type =  '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
+            isImage: function (file) {
+                var type = '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
                 return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
             }
         };
-
         return {
             restrict: 'A',
             template: '<canvas/>',
-            link: function(scope, element, attributes) {
+            link: function (scope, element, attributes) {
                 if (!helper.support) return;
 
                 var params = scope.$eval(attributes.ngThumb);
@@ -73,7 +72,7 @@ angular.module('app')
                 function onLoadImage() {
                     var width = params.width || this.width / this.height * params.height;
                     var height = params.height || this.height / this.width * params.width;
-                    canvas.attr({ width: width, height: height });
+                    canvas.attr({width: width, height: height});
                     canvas[0].getContext('2d').drawImage(this, 0, 0, width, height);
                 }
             }
