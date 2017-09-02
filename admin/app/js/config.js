331a649cd4c8f01ecde98618cb98737840ce68de
ahmdabos <ahmdabos@gmail.com>
2017-09-02 21:43:00 +0400

    working with jwt in for angular and CI

--- admin/app/js/config.js
+++ admin/app/js/config.js
@@ -1,16 +1,17 @@
 'use strict';
 angular.module('app')
-.constant('URL', {
-    baseApi: 'http://localhost/cing/restfull/api/',
-    articleApi: 'apiarticles',
-    uploadApi:'apiuploads',
-    authenticationsApi:'apiauthentications'
-})
-.config(['$httpProvider','$cookies', function($httpProvider,$cookies) {
-    if($cookies.get('currentUser')){
+    .constant('URL', {
+        baseApi: 'http://localhost/cing/restfull/api/',
+        articleApi: 'apiarticles',
+        uploadApi: 'apiuploads',
+        authenticationsApi: 'apiauthentications'
+    })
+
+    .run(function ($http, $cookies) {
         var currentUser = JSON.parse($cookies.get('currentUser'));
         var token = currentUser.token;
-        $httpProvider.defaults.headers.common['Authorization'] = 'Bearer ' + token;
-    }
-}])
-.value('_', window._);
+        $http.defaults.headers.common.Authorization = 'bearer ' + token;
+    })
+
+
+    .value('_', window._);
